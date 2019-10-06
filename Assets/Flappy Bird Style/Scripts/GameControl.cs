b9609715c79af74451ea40c67ce7ea72fd4bba5b
Lennart Fischer <lennart.fischer@adolfinum.de>
2019-10-06 10:00:48 +0200

    Added Health Status

--- Assets/Flappy Bird Style/Scripts/GameControl.cs
+++ Assets/Flappy Bird Style/Scripts/GameControl.cs
@@ -8,11 +8,12 @@ public class GameControl : MonoBehaviour
 	public static GameControl instance;			//A reference to our game control script so we can access it statically.
 	public Text scoreText;						//A reference to the UI text component that displays the player's score.
 	public GameObject gameOvertext;				//A reference to the object that displays the text which appears when the player dies.
+    public GameObject healthStatus;
 
 	private int score = 5;						//The player's score.
 	public bool gameOver = false;				//Is the game over?
 	public float scrollSpeed = -1.5f;
-
+    private int totalHealth = 5;
 
 	void Awake()
 	{
@@ -33,7 +34,15 @@ public class GameControl : MonoBehaviour
 		{
 			//...reload the current scene.
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-		}
+            // set visible
+            GameObject[] trees = GameObject.FindGameObjectsWithTag("Trees");
+
+            for (int i = 0; i < totalHealth; i++)
+            {
+                Renderer render = trees[i].GetComponent<Renderer>();
+                render.enabled = true;
+            }
+        }
 	}
 
     public void PlayerHitEnemy()
@@ -41,6 +50,21 @@ public class GameControl : MonoBehaviour
         score--;
 
         scoreText.text = "LP: " + score.ToString();
+
+        GameObject[] trees = GameObject.FindGameObjectsWithTag("Trees");
+
+        for (int i = 0; i < totalHealth; i++)
+        {
+            Renderer render = trees[i].GetComponent<Renderer>();
+            render.enabled = false;
+        }
+
+        for (int i = 0; i < score; i++)
+        {
+            Renderer render = trees[i].GetComponent<Renderer>();
+            render.enabled = true;
+        }
+
 
         if (score == 0)
         {
@@ -60,6 +84,7 @@ public class GameControl : MonoBehaviour
 		score++;
 		//...and adjust the score text.
 		scoreText.text = "Score: " + score.ToString();
+
 	}
 
 	public void BirdDied()
